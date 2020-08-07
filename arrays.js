72615d9d5cfe36518f5dd81b75b7a6b13bb5c5b8
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-07 23:51:29 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, add) {
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, add) {
+  return array
+}
